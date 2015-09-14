7d1b12121fbd348c3f7227a4b1408e4ca8bd9f89
Sean Lao <slao258@gmail.com>
2015-09-14 16:11:33 -0700

    finish 17.1

--- MainClass.cs
+++ MainClass.cs
@@ -6,7 +6,7 @@ namespace ctci
 	{
 		public static void Main(string[] args)
 		{
-			Q16_1.RunTests ();
+			Q17_1.RunTests ();
 		}
 	}
 }
---
+++ Q17_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ctci
+{
+	public class Q17_1
+	{
+		public static int Add(int a, int b)
+		{
+			while (b != 0) {
+				int sum = a ^ b;
+				int carry = (a & b) << 1;
+				a = sum;
+				b = carry;
+			}
+			return a;
+		}
+
+		public static void RunTests()
+		{
+			Console.WriteLine (Add(1, 1));
+			Console.WriteLine (Add(4, 0));
+			Console.WriteLine (Add(-1, 4));
+		}
+	}
+}
+
