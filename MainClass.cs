830eece6da47f689a31341f3ad0ffdf662225a33
Sean Lao <slao258@gmail.com>
2015-09-15 23:51:25 -0700

    finish 2.3

--- MainClass.cs
+++ MainClass.cs
@@ -6,7 +6,7 @@ namespace ctci
 	{
 		public static void Main(string[] args)
 		{
-			Q2_2.RunTests ();
+			Q2_3.RunTests ();
 		}
 	}
 }
---
+++ Q2_3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ctci
+{
+	public class Q2_3
+	{
+		public static bool DeleteNode(LinkedListNode<int> n)
+		{
+			if (n == null || n.next == null) {
+				return false;
+			}
+			n.data = n.next.data;
+			n.next = n.next.next;
+			return true;
+		}
+
+		public static void RunTests()
+		{
+			LinkedListNode<int> l = new LinkedListNode<int> (4, 5, 6, 5, 2, 1, 9, 2, 0);
+			Console.WriteLine (l);
+			Console.WriteLine (DeleteNode (l.next.next.next));
+			Console.WriteLine (l);
+			Console.WriteLine (DeleteNode (l.next.next.next));
+			Console.WriteLine (l);
+		}
+	}
+}
+
