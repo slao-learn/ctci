db9afa73780dda6275c3f2a7a24d0c2adb89f75d
Sean Lao <slao258@gmail.com>
2015-09-19 06:04:06 -0700

    add more tests to 5.3

--- Q5_3.cs
+++ Q5_3.cs
@@ -6,25 +6,25 @@ namespace ctci
 	{
 		public static int FlipBit(int n)
 		{
+			const int NUM_BITS = sizeof(int) * 8;
 			int count1 = 0, count2 = 0, max = 1;
-			bool flipped1 = false, flipped2 = false;
-			while (n != 0) {
+			bool flipped1 = false;
+			for (int i = 0; i < NUM_BITS; ++i)
+			{
 				if ((n & 1) == 1) {
 					++count1;
 					++count2;
 				} else if (!flipped1) {
 					flipped1 = true;
 					++count1;
-					flipped2 = false;
 					count2 = 0;
-				} else if (!flipped2) {
-					flipped2 = true;
-					++count2;
+				} else {
 					flipped1 = false;
+					++count2;
 					count1 = 0;
 				}
-				max = Math.Max (max, count1);
-				max = Math.Max (max, count2);
+				max = Math.Max (max, Math.Max(count1, count2));
+				if (n == 0) break;
 				n = (int)((uint)n >> 1);
 			}
 			return max;
@@ -33,8 +33,10 @@ namespace ctci
 		public static void RunTests()
 		{
 			Console.WriteLine (FlipBit(-1));
+			Console.WriteLine (FlipBit(int.MaxValue));
 			Console.WriteLine (FlipBit(-10));
 			Console.WriteLine (FlipBit(0));
+			Console.WriteLine (FlipBit(1));
 			Console.WriteLine (FlipBit(15));
 			Console.WriteLine (FlipBit(1775));
 		}
